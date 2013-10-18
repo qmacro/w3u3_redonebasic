c12ad1228dcb7ba7a583cb572c8e8aff38ec108b
DJ Adams <dj.adams@pobox.com>
2013-10-18 23:14:00 +0100

    Write JS view 'declaratively' :-)

--- myapp/ProductList.view.js
+++ myapp/ProductList.view.js
@@ -6,27 +6,24 @@ sap.ui.jsview("com.opensap.ProductList", {
 
     createContent: function(oController) {
 
-        var oList = new sap.m.List({
-            inset: false,
-            type: sap.m.ListType.DetailAndActive,
-            headerText: "Product Overview",
-        });
-
-        oList.bindItems({
-            path: "/ProductCollection",
-            template: new sap.m.StandardListItem({
-                title: "{Name}",
-                description: "{Description}",
-                type: sap.m.ListType.Navigation,
-                press: [oController.handleProductListItemPress, oController]
-            }),
-        });
-
         return new sap.m.Page("ProductPage", {
             title: "Products",
-            content: [ oList ]
-        })
-
+            content: [
+                new sap.m.List({
+                    headerText: "Product Overview",
+                    items: {
+                        path: "/ProductCollection",
+                        template: new sap.m.StandardListItem({
+                            title: "{Name}",
+                            description: "{Description}",
+                            type: sap.m.ListType.Navigation,
+                            press: [oController.handleProductListItemPress, oController]
+                        })
+                    }
+                })
+            ]
+        });
+        
     }
 
 });
